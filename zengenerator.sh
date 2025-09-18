05755bd663e1305642987d679db16fbef37ed414
Alex T <lexkie@proton.me>
2025-09-18 14:17:56 -0400

    Add files via upload

---
+++ zengenerator.sh
@@ -0,0 +1,26 @@
+
+
+echo "Press 'q' to exit zen generator."
+play -q "water.flac" repeat 1000000000 & play -q "rainx.flac" repeat 1000000000 & play -q "skiesx.flac" repeat 1000000000 &
+tibetan=0
+while true; do
+minmax=$(( (RANDOM % 5) + 1 ))
+bamboo=$(find /bamboo -type f | shuf -n 1)
+sleepdur="0."
+sleepdur+=$minmax
+play -q "$bamboo" vol "$sleepdur" &
+sleep $sleepdur
+sleepdur=""
+((tibetan++))
+if [[ $tibetan -gt 100 ]]; then
+bowls=$(find /tibetan -type f | shuf -n 1)
+play -q "$bowls" &
+tibetan=0
+fi
+read -n 1 -t 0.1 input
+    if [[ $input = "q" ]] || [[ $input = "Q" ]]; then
+        echo "Exiting loop."
+        pkill play
+        break # Exit the loop if 'q' is pressed
+    fi
+done
